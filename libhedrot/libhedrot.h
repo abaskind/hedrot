f34a70788161fcfe3ec0e954c764c3b6d0f3f147
Alexis Baskind <a@alexisbaskind.net>
2017-09-04 09:34:44 +0200

    v1.2.2RC

--- libhedrot/libhedrot.h
+++ libhedrot/libhedrot.h
@@ -22,7 +22,7 @@
 
 
 // hedrot version
-#define HEDROT_VERSION "1.2.2beta1"
+#define HEDROT_VERSION "1.2.2"
 
 //=====================================================================================================
 // static definitions
